be7f87098a065509573e0544bffc5d13cbb2c62e
Atmatm6 <atmatm6000@gmail.com>
2017-05-20 11:03:55 -0600

    first commit

---
+++ build/main.d
@@ -0,0 +1,205 @@
+main.o: /home/atmatm6/wiiudev/shana3ds/source/main.c \
+ /home/atmatm6/devkitPro/libctru/include/3ds.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/types.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/result.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/ipc.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/svc.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/srv.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/errf.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/os.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/synchronization.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/thread.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/gfx.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/gspgpu.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/console.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/env.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/util/utf.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/allocator/linear.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/allocator/mappable.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/allocator/vram.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/ac.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/am.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/fs.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/ampxi.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/apt.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/boss.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/cam.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/y2r.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/cfgnor.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/cfgu.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/csnd.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/dsp.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/gsplcd.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/hid.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/irrst.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/sslc.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/httpc.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/uds.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/ndm.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/nim.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/nwmext.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/ir.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/ns.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/pm.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/ps.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/ptmu.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/ptmsysm.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/pxidev.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/soc.h \
+ /home/atmatm6/devkitPro/libctru/include/netinet/in.h \
+ /home/atmatm6/devkitPro/libctru/include/sys/socket.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/mic.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/mvd.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/nfc.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/news.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/qtm.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/srvpm.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/services/hb.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/gpu/gx.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/gpu/gpu.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/gpu/registers.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/gpu/enums.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/gpu/shbin.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/gpu/shaderProgram.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/ndsp/ndsp.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/ndsp/channel.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/applets/swkbd.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/sdmc.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/romfs.h \
+ /home/atmatm6/devkitPro/libctru/include/3ds/font.h
+
+/home/atmatm6/devkitPro/libctru/include/3ds.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/types.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/result.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/ipc.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/svc.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/srv.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/errf.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/os.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/synchronization.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/thread.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/gfx.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/gspgpu.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/console.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/env.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/util/utf.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/allocator/linear.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/allocator/mappable.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/allocator/vram.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/ac.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/am.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/fs.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/ampxi.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/apt.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/boss.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/cam.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/y2r.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/cfgnor.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/cfgu.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/csnd.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/dsp.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/gsplcd.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/hid.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/irrst.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/sslc.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/httpc.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/uds.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/ndm.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/nim.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/nwmext.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/ir.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/ns.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/pm.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/ps.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/ptmu.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/ptmsysm.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/pxidev.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/soc.h:
+
+/home/atmatm6/devkitPro/libctru/include/netinet/in.h:
+
+/home/atmatm6/devkitPro/libctru/include/sys/socket.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/mic.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/mvd.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/nfc.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/news.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/qtm.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/srvpm.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/services/hb.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/gpu/gx.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/gpu/gpu.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/gpu/registers.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/gpu/enums.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/gpu/shbin.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/gpu/shaderProgram.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/ndsp/ndsp.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/ndsp/channel.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/applets/swkbd.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/sdmc.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/romfs.h:
+
+/home/atmatm6/devkitPro/libctru/include/3ds/font.h:
