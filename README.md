4f76fe7982dff1ae56614c179bc84f03a161b27f
Atmatm6 <atmatm6000@gmail.com>
2017-05-23 08:05:45 -0600

    many thing

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 A test project for me to use that can be build in the most normal way possible,
 using the template from devkitPro.
+
+## TODO
+
+0. ~~Complete the basics (create 3dsx for homebrew launcher)~~ **Completed**
+    - ~~Create a CIA file~~
+
+1. ~~Use the native keyboard (swkbd)~~ **Completed**
+    - ~~Get keyboard data and display on screen~~
+
+2. Create a calculator
+    - Use the keyboard's numpad to do simple math.
+    - Allow using addition, subtraction, multiplication, and division.
--- source/main.c
+++ source/main.c
@@ -2,19 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <3ds.h>
+
+void wose(){
+    
+}
+
 int main(int argc, char **argv) {
 	gfxInitDefault();
-	consoleInit(GFX_TOP, NULL);
+        PrintConsole topScreen, bottomScreen;
+	consoleInit(GFX_TOP, &topScreen);
+        consoleInit(GFX_BOTTOM, &bottomScreen);
         SwkbdState swkbd;
+        int prevShell = PTMU_GetShellState();
 	printf("Press A to start the keyboard.\n");
         printf("Press START to go back to the loader.\n");
         newsInit();
         char woah[30];
         swkbdInit(&swkbd, SWKBD_TYPE_QWERTY,3,30);
+        bool closed = false;
 	// Main loop
 	while (aptMainLoop()) {
-
-		gspWaitForVBlank();
+                if (prevShell != PTMU_GetShellState() & !closed){
+                    
+                }
+                gspWaitForVBlank();
 		hidScanInput();
 		u32 kDown = hidKeysDown();
 		if (kDown & KEY_START)
@@ -24,13 +35,9 @@ int main(int argc, char **argv) {
                     if (sb == SWKBD_BUTTON_CONFIRM) printf(woah, '\n');
                     else if(sb == SWKBD_BUTTON_LEFT) printf("Canceled\n");
                     else printf("I'm not sure what happened.\n");
-                }/* else if (kDown & KEY_B){
-                    char title[10] = "Shana3DS"; 
-                    char message[255] = "Hello there. I'm Omochao and I'll be your guide."; 
-                 dead for now
-                    NEWS_AddNotification((u16) atoi(title),(u32) sizeof(title),(u16) atoi(message),
-                            (u32) sizeof(message), MagickImage, sizeof(MagickImage),false);*/
-                //}
+                } else if (kDown & KEY_B){
+                    
+                }
 
 		// Flush and swap framebuffers
 		gfxFlushBuffers();
